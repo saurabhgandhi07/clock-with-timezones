c05953c05d8523439734d1eeddaea826724c8c29
Saurabh Gandhi <saurabh.gandhi@motifworks.com>
2021-05-16 20:25:29 +0530

    update:clock with respect to timezones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import moment_tz_ from 'moment-timezone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clock-with-timezones';
+  localTime: any;
+  time: any;
+  selectedTimeZone: any = "Asia/Calcutta";
+  showTime: any;
+  momentTz = moment_tz_;
+  timeZones: any[] = [
+    {
+      "lookupCode": "IST",
+      "lookupName": "Indian Standard Time",
+      "lookupDescription": "Asia/Calcutta"
+    },
+    {
+      "lookupCode": "EST",
+      "lookupName": "Eastern Standard Time",
+      "lookupDescription": "America/New_York"
+    },
+    {
+      "lookupCode": "CDT",
+      "lookupName": "Central Standard Time",
+      "lookupDescription": "America/Chicago"
+    },
+    {
+      "lookupCode": "MDT",
+      "lookupName": "Mountain Standard Time",
+      "lookupDescription": "America/Denver"
+    },
+    {
+      "lookupCode": "PDT",
+      "lookupName": "Pacific Standard Time",
+      "lookupDescription": "America/Los_Angeles"
+    },
+    {
+      "lookupCode": "AKDT",
+      "lookupName": "Alaskan Standard Time",
+      "lookupDescription": "America/Anchorage"
+    },
+    {
+      "lookupCode": "EST",
+      "lookupName": "Eastern Standard Time",
+      "lookupDescription": "America/New_York"
+    },
+    {
+      "lookupCode": "HADT",
+      "lookupName": "Hawaiian Standard Time",
+      "lookupDescription": "Pacific/Honolulu",
+    }
+  ];
+
+  ngOnInit(): void {
+    this.getCurrentDate();
+  }
+
+  getCurrentDate() {
+    setInterval(() => {
+      let timeZoneCode = this.selectedTimeZone;
+      this.localTime = this.getTimeZone(timeZoneCode);
+    }, 1000);
+  }
+
+  getTimeZone(tz: any) {
+    this.time = this.momentTz.tz(tz).format('hh:mm:ss A');
+    return this.time;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatToolbarModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
